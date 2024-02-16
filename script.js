f1ead2e0aa6fafca0ddd70e0cd2d9eeafc9b7ccd
tammeusb <tammeusb@gmail.com>
2024-02-16 10:32:48 -0500

    Solution 3: add ".indexOf" method
    Used .indexOf array method to return specific arrayElement
    rather than relatively returning it with a confusing iterative.

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const removeFromArray = function(array, ...removeElement) {
-    //loop to check if array still has any values from removeElement?
-    let i = 1;
-    removeElement = removeElement.sort()
+const removeFromArray = function(array, ...elementsToRemove) {
 
-    for (let a = 0; a < 10; a++) {
-        for (const element of removeElement) {
-            for (const arrayElement of array) {
-                if (element == arrayElement) {
-                    console.log(true);
-                    array = array.toSpliced(arrayElement - i, 1);
-                    console.log(array);
-                   //loop to check if any same left???
-                }
+    elementsToRemove = elementsToRemove.sort()
+
+    for (const removeElement of elementsToRemove) {
+        for (const arrayElement of array) {
+            if (removeElement === arrayElement) {
+                //console.log(array.indexOf(arrayElement));
+                //console.log(removeElement, arrayElement);
+                array = array.toSpliced(array.indexOf(arrayElement), 1);
+                //console.log(array);
             }
         }
     }
+
     return array
+
 }
