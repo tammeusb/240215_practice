4c60c2a387daa69855981eac7538f0399eef0ec2
tammeusb <tammeusb@gmail.com>
2024-02-16 11:08:24 -0500

    Add "ERROR" message for non-number parameters
    Typeof operator must return 'number' for conditional
    to pass on to the loop

--- exercise-05.js
+++ exercise-05.js
@@ -1,16 +1,19 @@
 const sumAll = function(unsortedOne, unsortedTwo) {
 
-    const args = Array.from(arguments);
-    args.sort();
+        const args = Array.from(arguments);
+        args.sort();
 
-    let numOne = args[0];
-    let numTwo = args[1];
+        let numOne = args[0];
+        let numTwo = args[1];
+
+    if (numOne < 0 || numTwo < 0 || typeof numOne != 'number' || typeof numTwo != 'number') 
+        return "ERROR";
 
     let sum = numOne;
 
-    for (numOne; numTwo > numOne; numOne++) {
-        sum = sum + (numOne + 1);
-    }
+        for (numOne; numTwo > numOne; numOne++) {
+            sum = sum + (numOne + 1);
+        }  
 
     return sum
 
